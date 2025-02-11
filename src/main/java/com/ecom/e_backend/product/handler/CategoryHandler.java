d1f0b7bca2650944ec390ae865788d08e0b12737
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-02-11 17:42:16 -0500

    Add security authorization to CategoryHandler methods for admin access

--- src/main/java/com/ecom/e_backend/product/handler/CategoryHandler.java
+++ src/main/java/com/ecom/e_backend/product/handler/CategoryHandler.java
@@ -3,6 +3,7 @@ package com.ecom.e_backend.product.handler;
 import java.util.UUID;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import com.ecom.e_backend.product.dto.CategoryDto;
 import com.ecom.e_backend.product.entity.Category;
 import com.ecom.e_backend.product.service.CategoryService;
+import com.ecom.e_backend.security.enums.Role;
 import com.ecom.e_backend.validation.ObjectValidator;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,7 @@ public class CategoryHandler {
 
     private final ObjectValidator objectValidator;
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public Mono<ServerResponse> save(ServerRequest request) {
         return request.bodyToMono(CategoryDto.class).doOnNext(objectValidator::validate)
                 .flatMap(dto -> ServerResponse.ok()
@@ -36,6 +39,7 @@ public class CategoryHandler {
                 .body(categoryService.findAll(), Category.class);
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public Mono<ServerResponse> deleteByPublicId(ServerRequest request) {
         UUID publicId = UUID.fromString(request.pathVariable("public_id"));
         return ServerResponse.ok()
